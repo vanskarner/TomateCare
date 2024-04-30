d32b43474193ca550f51f3bc36f0259031e81944
Vanskarner <vanskarner@gmail.com>
2024-04-29 23:07:43 -0500

    info screen design

--- app/src/main/java/com/vanskarner/tomatecare/MainActivity.kt
+++ app/src/main/java/com/vanskarner/tomatecare/MainActivity.kt
@@ -2,11 +2,19 @@ package com.vanskarner.tomatecare
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.vanskarner.tomatecare.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val infoDialog = InfoDialog()
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnInfo.setOnClickListener { infoDialog.show(supportFragmentManager) }
     }
 
 }
