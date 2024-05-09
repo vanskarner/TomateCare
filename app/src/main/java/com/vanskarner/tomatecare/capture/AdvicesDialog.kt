3ae510410c3ade0f9c3295e562447849dd7ab093
Vanskarner <vanskarner@gmail.com>
2024-05-09 17:55:42 -0500

    The dialogue of the councils has been transferred to their own class.

---
+++ app/src/main/java/com/vanskarner/tomatecare/capture/AdvicesDialog.kt
@@ -0,0 +1,45 @@
+package com.vanskarner.tomatecare.capture
+
+import android.app.Dialog
+import android.graphics.BitmapFactory
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.FragmentManager
+import com.bumptech.glide.Glide
+import com.vanskarner.tomatecare.databinding.DialogCaptureAdviceBinding
+
+class AdvicesDialog : DialogFragment() {
+    private val tag = "AdviceDialog"
+
+    fun show(fragmentManager: FragmentManager) {
+        super.show(fragmentManager, tag)
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val bindingAdvices = DialogCaptureAdviceBinding.inflate(layoutInflater)
+        val correctPlant = requireContext().assets.open("correct_plant.png")
+        val distantPlant = requireContext().assets.open("distant_plant.jpeg")
+        val plantNearby = requireContext().assets.open("plant_nearby.jpg")
+        val unfocusedPlant = requireContext().assets.open("unfocused_plant.jpg")
+        val accumulatedPlants = requireContext().assets.open("accumulated_plants.jpeg")
+        Glide.with(this)
+            .load(BitmapFactory.decodeStream(correctPlant))
+            .into(bindingAdvices.imvCorrectPlant)
+        Glide.with(this)
+            .load(BitmapFactory.decodeStream(distantPlant))
+            .into(bindingAdvices.imvDistantPlant)
+        Glide.with(this)
+            .load(BitmapFactory.decodeStream(plantNearby))
+            .into(bindingAdvices.imvPlantNearby)
+        Glide.with(this)
+            .load(BitmapFactory.decodeStream(unfocusedPlant))
+            .into(bindingAdvices.imvBlurredPlant)
+        Glide.with(this)
+            .load(BitmapFactory.decodeStream(accumulatedPlants))
+            .into(bindingAdvices.imvAccumulatedPlants)
+        val alertBuilder = AlertDialog.Builder(requireContext())
+        alertBuilder.setView(bindingAdvices.root)
+        return alertBuilder.create()
+    }
+}
--- app/src/main/java/com/vanskarner/tomatecare/capture/CaptureFragment.kt
+++ app/src/main/java/com/vanskarner/tomatecare/capture/CaptureFragment.kt
@@ -1,19 +1,15 @@
 package com.vanskarner.tomatecare.capture
 
-import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.activity.addCallback
-import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
-import com.bumptech.glide.Glide
 import com.vanskarner.tomatecare.BaseBindingFragment
 import com.vanskarner.tomatecare.MainViewModel
 import com.vanskarner.tomatecare.Selection
-import com.vanskarner.tomatecare.databinding.DialogCaptureAdviceBinding
 import com.vanskarner.tomatecare.databinding.FragmentCaptureBinding
 
 class CaptureFragment : BaseBindingFragment<FragmentCaptureBinding>() {
@@ -21,6 +17,7 @@ class CaptureFragment : BaseBindingFragment<FragmentCaptureBinding>() {
     private val viewModel: CaptureViewModel by viewModels()
     private val viewModelActivity: MainViewModel by activityViewModels()
     private val settingDialog = SettingDialog()
+    private val advicesDialog = AdvicesDialog()
 
     override fun inflateBinding(
         inflater: LayoutInflater,
@@ -35,7 +32,7 @@ class CaptureFragment : BaseBindingFragment<FragmentCaptureBinding>() {
             onBackPressed = { viewModelActivity.showBottomNavigation(Selection.Identification) })
         binding.btnCapture.setOnClickListener { goToIdentificationFragment() }
         binding.imvSettings.setOnClickListener { viewModel.getSetting() }
-        binding.tvTips.setOnClickListener { showAdvicesDialog() }
+        binding.tvTips.setOnClickListener { advicesDialog.show(childFragmentManager) }
         settingDialog.setOnApply {
             viewModel.updateSetting(it)
             settingDialog.dismiss()
@@ -51,34 +48,6 @@ class CaptureFragment : BaseBindingFragment<FragmentCaptureBinding>() {
     private fun goToIdentificationFragment() {
         val navDirection = CaptureFragmentDirections.toIdentificationFragment()
         findNavController().navigate(navDirection)
-    }
-
-    private fun showAdvicesDialog() {
-        val bindingAdvices = DialogCaptureAdviceBinding.inflate(layoutInflater)
-        val correctPlant = requireContext().assets.open("correct_plant.png")
-        val distantPlant = requireContext().assets.open("distant_plant.jpeg")
-        val plantNearby = requireContext().assets.open("plant_nearby.jpg")
-        val unfocusedPlant = requireContext().assets.open("unfocused_plant.jpg")
-        val accumulatedPlants = requireContext().assets.open("accumulated_plants.jpeg")
-        Glide.with(this)
-            .load(BitmapFactory.decodeStream(correctPlant))
-            .into(bindingAdvices.imvCorrectPlant)
-        Glide.with(this)
-            .load(BitmapFactory.decodeStream(distantPlant))
-            .into(bindingAdvices.imvDistantPlant)
-        Glide.with(this)
-            .load(BitmapFactory.decodeStream(plantNearby))
-            .into(bindingAdvices.imvPlantNearby)
-        Glide.with(this)
-            .load(BitmapFactory.decodeStream(unfocusedPlant))
-            .into(bindingAdvices.imvBlurredPlant)
-        Glide.with(this)
-            .load(BitmapFactory.decodeStream(accumulatedPlants))
-            .into(bindingAdvices.imvAccumulatedPlants)
-        val alertBuilder = AlertDialog.Builder(requireContext())
-        alertBuilder.setView(bindingAdvices.root)
-        val alert = alertBuilder.create()
-        alert.show()
     }
 
 }
