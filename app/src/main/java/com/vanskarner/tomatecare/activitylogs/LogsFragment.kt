2533ec55b247b47cf3420bfff165cbb361f999c9
Vanskarner <vanskarner@gmail.com>
2024-05-08 19:31:48 -0500

    update

--- app/src/main/java/com/vanskarner/tomatecare/activitylogs/LogsFragment.kt
+++ app/src/main/java/com/vanskarner/tomatecare/activitylogs/LogsFragment.kt
@@ -4,15 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.activity.addCallback
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.vanskarner.tomatecare.BaseBindingFragment
+import com.vanskarner.tomatecare.R
 import com.vanskarner.tomatecare.databinding.FragmentLogsBinding
 
 
 class LogsFragment : BaseBindingFragment<FragmentLogsBinding>() {
 
-    private val viewModel:LogsViewModel by viewModels()
+    private val viewModel: LogsViewModel by viewModels()
 
     override fun inflateBinding(
         inflater: LayoutInflater,
@@ -27,6 +29,7 @@ class LogsFragment : BaseBindingFragment<FragmentLogsBinding>() {
         requireActivity().onBackPressedDispatcher.addCallback(
             viewLifecycleOwner,
             onBackPressed = { goToStartFragment() })
+        binding.lyDelete.setOnClickListener { }
     }
 
     override fun setupViewModel() {
@@ -36,6 +39,22 @@ class LogsFragment : BaseBindingFragment<FragmentLogsBinding>() {
     private fun goToStartFragment() {
         val direction = LogsFragmentDirections.toStartFragment()
         findNavController().navigate(direction)
+    }
+
+    fun showDeleteDialog(onAccept: () -> Unit) {
+        val builderDialog = AlertDialog.Builder(requireContext())
+        builderDialog.apply {
+            setMessage(R.string.delete_selected_items)
+            setPositiveButton(R.string.delete) { dialog, _ ->
+                onAccept()
+                dialog.dismiss()
+            }
+            setNegativeButton(R.string.cancel) { dialog, _ ->
+                dialog.dismiss()
+            }
+        }
+        val dialog = builderDialog.create()
+        dialog.show()
     }
 
 }
