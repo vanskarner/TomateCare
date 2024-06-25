9d6a77c72edd3113375936665390464c61b39001
LuisOlazo <luisolazolujan@gmail.com>
2024-06-25 18:30:39 -0500

    FindAnalysisUseCase Development and Testing

---
+++ analysisTracking/src/main/java/com/vanskarner/analysistracking/bussineslogic/FindAnalysisUseCase.kt
@@ -0,0 +1,28 @@
+package com.vanskarner.analysistracking.bussineslogic
+
+import com.vanskarner.analysistracking.AnalysisDetailData
+import com.vanskarner.diseases.DiseasesComponent
+
+internal class FindAnalysisUseCase(
+    private val repository: Repository,
+    private val diseasesComponent: DiseasesComponent
+) {
+
+    suspend fun execute(id: Int): Result<AnalysisDetailData> = runCatching {
+        val result = repository.findAnalysis(id).getOrThrow()
+        val formattedResult = result.copy(
+            classificationData = result.classificationData.map { item ->
+                val keyCode = item.bestPrediction
+                val keyCodes = item.predictions.map { prediction -> prediction.first }
+                val realName = diseasesComponent.getNameByKeyCode(keyCode.first).getOrThrow()
+                val realNames = diseasesComponent.getNamesByKeyCodes(keyCodes).getOrThrow()
+                item.copy(
+                    bestPrediction = Pair(realName, item.bestPrediction.second),
+                    predictions = item.predictions.zip(realNames) { v1, v2 -> v1.copy(first = v2) }
+                )
+            }
+        )
+        return Result.success(formattedResult)
+    }
+
+}
