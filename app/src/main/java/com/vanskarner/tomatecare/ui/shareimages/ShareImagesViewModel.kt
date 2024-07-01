a7a7fa813eea071114e021e7a1af38a7154050ce
LuisOlazo <luisolazolujan@gmail.com>
2024-06-30 19:11:19 -0500

    The option to share images is hidden for reasons of time

--- app/src/main/java/com/vanskarner/tomatecare/ui/shareimages/ShareImagesViewModel.kt
+++ app/src/main/java/com/vanskarner/tomatecare/ui/shareimages/ShareImagesViewModel.kt
@@ -5,5 +5,4 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class ShareImagesViewModel @Inject constructor(): ViewModel() {
-}
+class ShareImagesViewModel @Inject constructor(): ViewModel()
